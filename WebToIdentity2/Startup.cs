bb0766993918e1072afbf73a08e9f8133ecccd31
Administrator <Administrator@WIN-U52NHKBA2NB>
2016-06-21 16:25:13 +0800

    加入專案檔案。

---
+++ WebToIdentity2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebToIdentity2.Startup))]
+namespace WebToIdentity2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
